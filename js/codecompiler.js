7d3bd82379861115dfe920e5c17090128f64ff73
love143purohit <36108717+love143purohit@users.noreply.github.com>
2018-03-01 19:05:07 +0530

    File for calling compiler api
    update

---
+++ js/codecompiler.js
@@ -0,0 +1,34 @@
+function UserAction() {
+
+	//Accessing the editor
+	var editor = ace.edit("codeEditor");
+	
+	//Getting text form the editor
+	var text = editor.getValue();
+    
+	//Forming http request
+	var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "https://api.jdoodle.com/v1/execute", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    
+	//OnResponse from the server
+	xhttp.onreadystatechange = function () {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+        var json = JSON.parse(xhttp.responseText);
+		document.getElementById('output').innerHTML = json.output;
+		console.log(json.output + ", " + json.statusCode);
+    }
+};
+
+	//Creating data
+	var data = JSON.stringify({
+		"script":text,
+		"language":"c",
+		"versionIndex":"0",
+		"clientId":"69cdf4df5bd85a70a0bade64fbd66ecf",
+		"clientSecret":"f902c22c3cb7549296ea02f72c9de8f81c8b3cec71da0bc86d2f030816214eec"
+	});
+
+	//Sending request to the server
+    xhttp.send(data);
+}
