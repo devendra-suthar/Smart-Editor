f5a212c824ddadca4df9c1314de152e4257e763b
love143purohit <36108717+love143purohit@users.noreply.github.com>
2018-03-07 19:22:24 +0530

    Updated code with google sign in

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(function() {
+
+  // Initialize Firebase
+   var config = {
+     apiKey: "AIzaSyDYi2iMsHGp03cwPtmMs5pIMahwwL-ePMI",
+     authDomain: "smart-editor-88913.firebaseapp.com",
+     databaseURL: "https://smart-editor-88913.firebaseio.com",
+     projectId: "smart-editor-88913",
+     storageBucket: "smart-editor-88913.appspot.com",
+     messagingSenderId: "630430771720"
+   };
+   firebase.initializeApp(config);
+}());
--- js/login.js
+++ js/login.js
@@ -7,3 +7,62 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+function logIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  window.location.assign("home.html");
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(user.email);
+  console.log(user.displayName);
+  window.console.log(user);
+
+  //var details = document.getElementById("details");
+  //details.innerHTML = user.displayName;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+}
+
+function logOut(){
+  //firebase.auth().signOut();
+  firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+  window.location.assign("index.html");
+  console.log("Signed out");
+}).catch(function(error) {
+  // An error happened.
+});
+}
+
+function getDetails(){
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var user = firebase.auth().currentUser;
+
+      if(user != null){
+        console.log("Signed In");
+        var email_id = user.email;
+        var name = user.displayName;
+        document.getElementById("details").innerHTML = "Welcome User : " + name;
+      }
+
+    } else {
+      // No user is signed in.
+      window.location.assign("index.html");
+      console.log("Not signed in");
+    }
+  });
+}
