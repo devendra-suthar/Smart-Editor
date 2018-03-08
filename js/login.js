113c51169b516b96e189e04a11cfad79358d1515
love143purohit <36108717+love143purohit@users.noreply.github.com>
2018-03-08 10:32:15 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -35,6 +35,23 @@ function logIn(){
 });
 }
 
+function loginWithEmail(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
 function logOut(){
   //firebase.auth().signOut();
   firebase.auth().signOut().then(function() {
