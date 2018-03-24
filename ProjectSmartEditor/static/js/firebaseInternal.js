bd8cf3ef0632eeddff48430b97c44c5964e3c5a6
lovepurohit <37335848+lovepurohit@users.noreply.github.com>
2018-03-24 10:35:58 +0530

    updated

--- ProjectSmartEditor/static/js/firebaseInternal.js
+++ ProjectSmartEditor/static/js/firebaseInternal.js
@@ -417,7 +417,7 @@ function readInbox(user_id) {
     var badgeValue = 0;
     var inbox = document.getElementById('inbox');
     var ref = firebase.database().ref('Inbox/' + user_id);
-    ref.on("value", function (snapshot) {
+    ref.once("value", function (snapshot) {
         snapshot.forEach(function (childSnapshot) {
             var childKey = childSnapshot.key;
             var childData = childSnapshot.val();
